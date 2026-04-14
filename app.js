4e40d553315869f3b01a8e512e35154fe3457018
Ernest <epacocha4@gmail.com>
2026-04-14 08:54:13 +0200

    feat: dodano oznaczanie zada jako ukonczonych

--- app.js
+++ app.js
@@ -15,3 +15,30 @@ form.addEventListener("submit", function (e) {
     input.value = ""; 
   }
 });
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const value = input.value.trim();
+
+  if (value !== "") {
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const span = document.createElement("span");
+    span.textContent = value;
+
+    // reakcja na zaznaczenie
+    checkbox.addEventListener("change", function () {
+      li.classList.toggle("done");
+    });
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+
+    list.appendChild(li);
+    input.value = "";
+  }
+});
